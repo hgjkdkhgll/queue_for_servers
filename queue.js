85ef72fd2e9dfd833b9dc44ad5ce77a48dfdf0fa
unknown <mahmood.hussein.basha@gmail.com>
2022-08-12 00:18:00 +0300

    add logging and change hit to interval

--- queue.js
+++ queue.js
@@ -7,23 +7,32 @@ var qc1 = queue({ results: [] })
 var qc2 = queue({ results: [] })
 var qo1 = queue({ results: [] })
 var qo2 = queue({ results: [] })
-let catalog1 = "localhost:6000";
-let catalog2 = "localhost:6001";
+let catalog1 = "localhost:15000";
+let catalog2 = "localhost:15000";
 let order1 = "localhost:6100";
-let order2 = "localhost:6101";
-app.post('/category/:id', (req) => {
+let order2 = "localhost:6100";
+app.post('/catalog/:id', (req,res) => {
     if (req.params.id == "1") {
         qc1.push(req.body);
+        console.log("catalog1");
     } else if (req.params.id == "2") {
         qc2.push(req.body);
+        console.log("catalog2");
     }
+    hit();
+    res.sendStatus(200);
 });
-app.post('/order/:id', (req) => {
+app.post('/order/:id', (req,res) => {
     if (req.params.id == "1") {
         qo1.push(req.body);
+        console.log("order1");
+
     } else if (req.params.id == "2") {
         qo2.push(req.body);
+        console.log("order2");
     }
+    hit();
+    res.sendStatus(200);
 });
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
@@ -31,12 +40,17 @@ app.listen(3000, () => {
 process.on('uncaughtException', function (err) {
     console.log('Caught exception: ', err);
 });
-//node js send post request every 5 seconds 
 
-setInterval(() => {
+
+async function hit(){
+    console.log("hit");
+    console.log(qc1.length);
+    console.log(qc2.length);
+    console.log(qo1.length);
+    console.log(qo2.length);
     if (qc1.length > 0) {
         let data = qc1.pop();
-        axios.post('http://' + catalog1 + '/api/sync', data).then((res) => {
+        axios.post('http://' + catalog2 + '/api/Books', data).then((res) => {
             if (res.status == 200) {
                 console.log("success");
             } else {
@@ -49,7 +63,7 @@ setInterval(() => {
             .then(() => {
                 if (qc2.length > 0) {
                     let data = qc2.pop();
-                    axios.post('http://' + catalog2 + '/api/sync', data).then((res) => {
+                    axios.post('http://' + catalog1 + '/api/Books', data).then((res) => {
                         if (res.status == 200) {
                             console.log("success");
                         } else {
@@ -62,7 +76,7 @@ setInterval(() => {
                         .then(() => {
                             if (qo1.length > 0) {
                                 let data = qo1.pop();
-                                axios.post('http://' + order1 + '/api/sync', data).then((res) => {
+                                axios.post('http://' + order2 + '/api/sync', data).then((res) => {
                                     if (res.status == 200) {
                                         console.log("success");
                                     } else {
@@ -75,7 +89,7 @@ setInterval(() => {
                                     .then(() => {
                                         if (qo2.length > 0) {
                                             let data = qo2.pop();
-                                            axios.post('http://' + order2 + '/api/sync', data).then((res) => {
+                                            axios.post('http://' + order1 + '/api/sync', data).then((res) => {
                                                 if (res.status == 200) {
                                                     console.log("success");
                                                 } else {
@@ -92,4 +106,4 @@ setInterval(() => {
                 }
             });
     }
-}, 5000);
+}
